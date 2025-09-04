91b971ca6b10af28ced6229a0ff985cf0d0bbb3a
Douglas <douglasmsrodrigues@hotmail.com>
2025-09-04 12:13:10 -0300

    Comecando do zero

--- src/components/Titulo/index.tsx
+++
@@ -1,5 +0,0 @@
-import { Titulo } from './styles'
-
-const TituloPrincipal = () => <Titulo>Lista de contatos</Titulo>
-
-export default TituloPrincipal
--- src/components/Titulo/styles.ts
+++
@@ -1,7 +0,0 @@
-import { styled } from 'styled-components'
-
-export const Titulo = styled.h2`
-  display: block;
-  padding-bottom: 16px;
-  padding-top: 6px;
-`
